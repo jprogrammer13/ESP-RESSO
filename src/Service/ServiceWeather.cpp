6a0ea1e801027df47829829ec5031d82957a07f1
davidecerpelloni <94609747+davidecerpelloni@users.noreply.github.com>
2021-11-29 21:26:21 +0100

    ServiceWeather

--- src/Service/ServiceWeather.cpp
+++ src/Service/ServiceWeather.cpp
@@ -56,39 +56,46 @@ String ServiceWeather::sendMsg(String msg)
         {
             Serial.print("deserializeJson() failed: ");
             Serial.println(error.c_str());
-            return;
+            return "error";
         }
 
-        long list_item_sunrise;
-        long list_item_sunset;
-        float list_item_temp_day;
-        float list_item_temp_min;
-        float list_item_temp_max;
-        float list_item_temp_night;
+        long list_item_sunrise[data_n_day];
+        long list_item_sunset[data_n_day];
+        float list_item_temp_day[data_n_day];
+        float list_item_temp_min[data_n_day];
+        float list_item_temp_max[data_n_day];
+        float list_item_temp_night[data_n_day];
+        float list_item_temp_eve[data_n_day];
+        float list_item_temp_morn[data_n_day];
 
         for (JsonObject list_item : response["list"].as<JsonArray>())
         {
-            list_item_sunrise = list_item["sunrise"]; // 1638167700, 1638254172, 1638340644
-            list_item_sunset = list_item["sunset"];   // 1638199986, 1638286356, 1638372729
+            for (int nth_day; nth_day < data_n_day; nth_day++)
+            {
+                list_item_sunrise[nth_day] = list_item["sunrise"]; // 1638167700, 1638254172, 1638340644
+                list_item_sunset[nth_day] = list_item["sunset"];   // 1638199986, 1638286356, 1638372729
 
-            JsonObject list_item_temp = list_item["temp"];
-            list_item_temp_day = list_item_temp["day"];     // 6.75, 6.01, 8.54
-            list_item_temp_min = list_item_temp["min"];     // -1.44, -0.38, 2.26
-            list_item_temp_max = list_item_temp["max"];     // 6.84, 6.87, 8.54
-            list_item_temp_night = list_item_temp["night"]; // 0.77, 2.54, 3.12
-            float list_item_temp_eve = list_item_temp["eve"];     // 4.34, 2.45, 3.05
-            float list_item_temp_morn = list_item_temp["morn"];   // 2.28, -0.27, 2.34
+                JsonObject list_item_temp = list_item["temp"];
+                list_item_temp_day[nth_day] = list_item_temp["day"];     // 6.75, 6.01, 8.54
+                list_item_temp_min[nth_day] = list_item_temp["min"];     // -1.44, -0.38, 2.26
+                list_item_temp_max[nth_day] = list_item_temp["max"];     // 6.84, 6.87, 8.54
+                list_item_temp_night[nth_day] = list_item_temp["night"]; // 0.77, 2.54, 3.12
+                list_item_temp_eve[nth_day] = list_item_temp["eve"];     // 4.34, 2.45, 3.05
+                list_item_temp_morn[nth_day] = list_item_temp["morn"];   // 2.28, -0.27, 2.34
 
-            int list_item_humidity = list_item["humidity"]; // 48, 34, 56
+                int list_item_humidity = list_item["humidity"]; // 48, 34, 56
 
-            JsonObject list_item_weather_0 = list_item["weather"][0];
-            const char *list_item_weather_0_main = list_item_weather_0["main"];               // "Clouds", "Clear", "Clear"
- 
+                JsonObject list_item_weather_0 = list_item["weather"][0];
+                const char *list_item_weather_0_main = list_item_weather_0["main"]; // "Clouds", "Clear", "Clear"
+            }
         }
+        
     }
     else
     {
     }
     // Free resources
     http.end();
+
+
 }
