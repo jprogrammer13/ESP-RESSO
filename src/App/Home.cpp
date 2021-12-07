aa7551e2a167541bcd01e336b59ee0ed1db01e96
Riccardo Bussola <buss.riky@gmail.com>
2021-12-07 14:24:35 +0100

    initial home implementation

--- src/App/Home.cpp
+++ src/App/Home.cpp
@@ -4,7 +4,11 @@
 
 Home::Home(Navigation *_navigation, Display *_display, SystemUi *_sysUi, SystemServices *_sysService) : App(_navigation, _display, _sysUi, _sysService)
 {
-    radius = 6;
+
+    hour = 15;
+    minute = 18;
+    day_week = "Mon";
+    day = 32;
 }
 
 void Home::draw()
@@ -12,25 +16,48 @@ void Home::draw()
     display->firstPage();
     do
     {
-        display->setFont(u8g2_font_ncenB14_tr);
-        display->setCursor(30, 45);
-        display->print("Hell   :)");
-        display->drawCircle(75, 38, radius);
+        display->setFontMode(1);
+        display->setDrawColor(1);
+
+        // display->drawDithering(0, 0, 128, 64, 75, 1);
+        display->drawBox(55, 0, 73, 64);
+        display->setFont(u8g2_font_7x14B_tf);
+        // display->drawRBox(3, 45, 35, 14, 7);
+        display->setDrawColor(0);
+        display->setFont(u8g2_font_helvR24_te);
+        display->drawStr(60, 28, (((hour < 10) ? ("0" + String(hour)) : String(hour)).c_str()));
+        // display->drawStr(90, 58, (((minute < 10) ? ("0" + String(minute)) : String(minute)).c_str()));
+        display->setFont(u8g2_font_7x14B_tf);
+
+        display->drawStr(105, 13, day_week.c_str());
+        display->drawStr(112, 26, (((day < 10) ? ("0" + String(day)) : String(day)).c_str()));
+
+        display->drawBox(0, 50, 128, 14);
+
+        display->setDrawColor(1);
+
+        for (int i = 2; i < 128; i += 3)
+        {
+            display->drawLine(i, 52, i, 62);
+        }
+
     } while (display->nextPage());
 
     switch (navigation->get_action())
     {
     case A_DOWN:
-        radius++;
+
         break;
     case A_UP:
-        radius--;
+
         break;
     case A_SELECT:
-        radius = 6;
+
         break;
     case A_BACK_LONG:
-        sysUi->setUi("Test");
+
         break;
     }
 }
+
+void Home::background() {}
